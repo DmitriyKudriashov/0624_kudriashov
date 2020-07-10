368da132a2ba6553a3d3a40f2c1766b833d4bf94
Кудряшов Дмитрий <67386647+DmitriyKudriashov@users.noreply.github.com>
2020-07-10 08:12:18 +0300

    Create botForYandex.js
    по сути отличие от бота для гугла лишь в двух строках:
    document.getElementById("text").value = "Гобой";
    document.getElementsByClassName("button")[0].click();

---
+++ botForYandex.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         botForYandex
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://yandex.ru/*
+// @grant        none
+// ==/UserScript==
+
+let keywords = ["Гобой","Как звучит флейта", "Тромбон", "Что такое валторна", "Фагот", "Скрипка", "Виолончель "];
+let keyword = keywords[getRandom(0,keywords.length)];
+let btnK = document.getElementsByClassName("button")[0];
+function getRandom(min,max){
+    return Math.floor(Math.random()*(max-min)+min);
+}
+
+function writeKeyWord(word){
+    let i=0
+    let timerID = setInterval(()=>{
+        document.getElementById("text").value+=word[i];
+        i++
+        if (i==word.length){
+            clearInterval(timerID);
+            btnK.click();
+        }
+    },1000)
+}
+
+if (btnK != undefined)
+    writeKeyWord(keyword);
+else {
+let links = document.links;
+    for (let i=0; i<links.length; i++){
+        if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")!=-1){
+            links[i].click();
+            break;
+        }
+    }
+}
